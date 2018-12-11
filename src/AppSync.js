f0b2b22a2ec63eb2fd5d6780038e34decbc40cc7
Jesús Federico <jefp@amazon.com>
2018-12-10 21:19:24 -0300

    Removed endpoints and keys

--- src/AppSync.js
+++ src/AppSync.js
@@ -1,6 +1,6 @@
 export default {
-  "graphqlEndpoint": "https://jhg7gmouijhhrk2snuerlxef7q.appsync-api.us-east-1.amazonaws.com/graphql",
+  "graphqlEndpoint": "APPSYNC_ENDPOINT",
   "region": "us-east-1",
   "authenticationType": "API_KEY",
-  "apiKey": "da2-zk4fbtjki5b7fjudzx3t472ip4"
+  "apiKey": "APPSYNC_KEY"
 }
